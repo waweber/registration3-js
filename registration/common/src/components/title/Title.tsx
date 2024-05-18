760e581fa4e651ff5b43f26890ec31fc170d9f31
Wes <wes@anthronewengland.com>
2024-05-18 12:20:59 -0400

    Improve title

--- registration/common/src/components/title/Title.tsx
+++ registration/common/src/components/title/Title.tsx
@@ -1,4 +1,4 @@
-import { IObservableValue, action, makeAutoObservable, observable } from "mobx"
+import { makeAutoObservable, reaction } from "mobx"
 import {
   ReactNode,
   createContext,
@@ -8,26 +8,61 @@ import {
 } from "react"
 
 class TitleState {
-  title: IObservableValue<readonly [string | null, string | null]>[] = []
+  entries: (readonly [string | null, string | null])[] = []
 
   constructor() {
     makeAutoObservable(this)
+
+    reaction(
+      () => this.title,
+      (title) => {
+        if (title) {
+          document.title = title
+        }
+      },
+    )
+  }
+
+  setTitle(level: number, title: string | null, subtitle: string | null) {
+    this.entries[level] = [title, subtitle]
+  }
+
+  unsetTitle(level: number) {
+    delete this.entries[level]
+    this.trim()
+  }
+
+  private trim() {
+    for (let i = this.entries.length - 1; i <= 0; i--) {
+      if (!this.entries[i]) {
+        this.entries.splice(i, 1)
+      }
+    }
+  }
+
+  get title(): string | null {
+    return this.entries.reduceRight(
+      (p, c) => (p ? p : c[0]),
+      null as string | null,
+    )
+  }
+
+  get subtitle(): string | null {
+    return this.entries.reduceRight(
+      (p, c) => (p ? p : c[1]),
+      null as string | null,
+    )
   }
 }
 
-const TitleContext = createContext<TitleState>(new TitleState())
+const TitleContext = createContext<readonly [TitleState, number]>([
+  new TitleState(),
+  0,
+])
 
 export const useTitle = (): readonly [string | null, string | null] => {
-  const ctx = useContext(TitleContext)
-  const title = ctx.title.reduceRight(
-    (p, c) => (p ? p : c.get()[0]),
-    null as string | null,
-  )
-  const subtitle = ctx.title.reduceRight(
-    (p, c) => (p ? p : c.get()[1]),
-    null as string | null,
-  )
-  return [title, subtitle]
+  const [ctx] = useContext(TitleContext)
+  return [ctx.title, ctx.subtitle]
 }
 
 export const Title = ({
@@ -39,29 +74,18 @@ export const Title = ({
   title?: string
   subtitle?: string
 }) => {
-  const state = useContext(TitleContext)
-  const [localState] = useState(() =>
-    observable.box([title ?? null, subtitle ?? null] as const),
+  const [state, level] = useContext(TitleContext)
+
+  const newValue = [state, level + 1] as const
+
+  useLayoutEffect(() => {
+    state.setTitle(level, title || null, subtitle || null)
+    return () => {
+      state.unsetTitle(level)
+    }
+  }, [level, title, subtitle])
+
+  return (
+    <TitleContext.Provider value={newValue}>{children}</TitleContext.Provider>
   )
-
-  useLayoutEffect(
-    action(() => {
-      state.title.splice(0, 0, localState)
-
-      return action(() => {
-        const idx = state.title.findIndex((v) => v == localState)
-        state.title.splice(idx, 1)
-      })
-    }),
-    [],
-  )
-
-  useLayoutEffect(
-    action(() => {
-      localState.set([title ?? null, subtitle ?? null])
-    }),
-    [title, subtitle],
-  )
-
-  return children
 }
