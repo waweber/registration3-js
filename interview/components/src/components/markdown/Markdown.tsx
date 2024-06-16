445af69fb8708e62c9863a6e963633ddde0b831d
Wes <wes@anthronewengland.com>
2024-06-16 18:18:05 -0400

    Update markdown links to target _blank

--- interview/components/src/components/markdown/Markdown.tsx
+++ interview/components/src/components/markdown/Markdown.tsx
@@ -2,12 +2,28 @@ import { Box, BoxProps, createPolymorphicComponent } from "@mantine/core"
 import clsx from "clsx"
 import { createContext, forwardRef, useContext, useMemo } from "react"
 import markdownit from "markdown-it"
+import { RenderRule } from "markdown-it/lib/renderer.mjs"
 
-export const MarkdownContext = createContext(
-  markdownit({
-    linkify: true,
-  }),
-)
+const defaultMarkdown = markdownit({ linkify: true })
+const defaultRenderer: RenderRule = (tokens, idx, options, env, self) =>
+  self.renderToken(tokens, idx, options)
+const defaultLinkOpenRenderer =
+  defaultMarkdown.renderer.rules.link_open ?? defaultRenderer
+
+const linkOpenRenderer: RenderRule = (tokens, idx, options, env, self) => {
+  const href = tokens[idx].attrGet("href")
+  const url = new URL(href || "", window.location.href)
+  const curUrl = new URL(window.location.href)
+  if (url.origin != curUrl.origin) {
+    tokens[idx].attrSet("target", "_blank")
+    tokens[idx].content += "&#x2934;"
+  }
+  return defaultLinkOpenRenderer(tokens, idx, options, env, self)
+}
+
+defaultMarkdown.renderer.rules.link_open = linkOpenRenderer
+
+export const MarkdownContext = createContext(defaultMarkdown)
 
 export type MarkdownProps = BoxProps & {
   children?: string
