7a21cee7b677023e7587225eba336721d6a6e432
Wes <wes@anthronewengland.com>
2024-09-02 16:14:41 -0400

    Update interview lib to use schema input lib

--- interview/lib/src/index.ts
+++ interview/lib/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./types.js"
-export * from "./schema/index.js"
 export { makeInterviewResponseStore } from "./store.js"
 export { makeInterviewAPI, getErrorResponse } from "./api.js"
 export { makeMockAPI } from "./mock.js"
